8b4ccc99436f0d5088f0c0a54e47f5c598e1f5c2
gelj <378720480@qq.com>
2019-11-14 16:50:57 +0800

    modify

--- src/main/java/com/project/miu/controller/CouponsListController.java
+++ src/main/java/com/project/miu/controller/CouponsListController.java
@@ -7,8 +7,6 @@ import com.project.miu.bean.utils.ResultUtil;
 import com.project.miu.commons.myEnum.ResultEnum;
 import com.project.miu.service.CouponsListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
